4b21e917cc4da6a88dc55bc95e89c1766f1524d5
golkedj <golkedj@gmail.com>
2020-02-22 21:30:39 -0700

    Added loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>('https://ng-complete-guide-golkedj.firebaseio.com/posts.json')
       .pipe(
@@ -53,7 +55,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
-        // ...
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
